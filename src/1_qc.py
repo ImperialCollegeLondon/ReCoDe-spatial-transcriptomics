74111232f89ca6483377ca776bc2756ca1e879fa
Sara Patti <sep22@ic.ac.uk>
2025-05-20 19:23:42 -0400

    updated path names and checked they worked, updated packages to include logging messages, added logging messages

--- src/1_qc.py
+++ src/1_qc.py
@@ -1,4 +1,8 @@
 # Import packages
+import warnings  # ? what is the best way to suppress warnings from package inputs?
+
+warnings.filterwarnings("ignore")
+
 import numpy as np
 
 import spatialdata as sd
@@ -10,55 +14,76 @@ import seaborn as sns
 import scanpy as sc
 import squidpy as sq
 
-import random
+from pathlib import Path
+
 import torch
 import os
 
-from helper_function.py import seed_everything
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[logging.FileHandler("my_script.log"), logging.StreamHandler()],
+)
+
+
+# from helper_function import seed_everything # ? not working please help
 
 # Set seed
-seed_everything(21122023)
+# seed_everything(21122023) # ? not working please help
 
 # Set variables
+# ? How should I config this so a user can easily change them?
 min_counts = 10
 min_cells = 5
 
 
-# Set directories
-input_path = "./"
-output_path = "./"
-xenium_path = f"{input_path}, /Xenium" # ^ Change to file path rather than f" string
-zarr_path = f"{output_path}, /Xenium.zarr" # ^ Change to file path rather than f" string
+# Set directories -
+input_path = "/Users/sarapatti/Desktop/PhD_projects/Llyod_lab/ReCoDe-spatial-transcriptomics"
+output_path = "/Users/sarapatti/Desktop/PhD_projects/Llyod_lab/ReCoDe-spatial-transcriptomics/analysis"
+xenium_path = Path(input_path) / "data/xenium"
+zarr_path = Path(input_path) / "xenium.zarr"
 
-# Import data
+# Load into spatialdata format
+logging.info("Reading Xenium data...")
 sdata = xenium(xenium_path)
 
-# Write to .zarr
-xenium.write(zarr_path)
+# Convert to zarr format
+logging.info("Writing to Zarr...")
+sdata.write(zarr_path)
 
 # Read in .zarr
-sdata = sd.read_zarr(zarr_path) #  read directly from the zarr store
+logging.info("Loading Xenium data...")
+sdata = sd.read_zarr(zarr_path)  #  read directly from the zarr store
 
+logging.info("Done")
 
-# Save anndata oject (stored in patialdata.tables layer) 
-adata = sdata.tables["table"] # contains the count matrix, cell and gene annotations
+# # Save anndata oject (stored in spatialdata.tables layer)
+adata = sdata.tables[
+    "table"
+]  # contains the count matrix, cell and gene annotations
 
-# $ Calculate and plot metrics 
+# $ Calculate and plot metrics
 
 # Calculate quality control metrics
 sc.pp.calculate_qc_metrics(adata, percent_top=(10, 20, 50, 150), inplace=True)
 
 # Calculate percent negative DNA probe and percent negative decoding count
 cprobes = (
-    adata.obs["control_probe_counts"].sum() / adata.obs["total_counts"].sum() * 100
+    adata.obs["control_probe_counts"].sum()
+    / adata.obs["total_counts"].sum()
+    * 100
 )
 cwords = (
-    adata.obs["control_codeword_counts"].sum() / adata.obs["total_counts"].sum() * 100
+    adata.obs["control_codeword_counts"].sum()
+    / adata.obs["total_counts"].sum()
+    * 100
 )
 print(f"Negative DNA probe count % : {cprobes}")
 print(f"Negative decoding count % : {cwords}")
 
-# Calculate averages 
+# Calculate averages
 avg_total_counts = np.mean(adata.obs["total_counts"])
 print(f"Average number of transcripts per cell: {avg_total_counts}")
 
@@ -69,7 +94,6 @@ area_max = np.max(adata.obs["cell_area"])
 area_min = np.min(adata.obs["cell_area"])
 print(f"Max cell area: {area_max}")
 print(f"Min cell area: {area_min}")
-
 
 
 # Plot
@@ -113,9 +137,8 @@ sc.pp.filter_genes(adata, min_cells=min_cells)
 
 
 # Normalize data
-adata.layers["counts"] = adata.X.copy() # make copy of raw data
-sc.pp.normalize_total(adata, inplace=True) # normalize data
-
+adata.layers["counts"] = adata.X.copy()  # make copy of raw data
+sc.pp.normalize_total(adata, inplace=True)  # normalize data
 # Log transform data
 sc.pp.log1p(adata)
 
