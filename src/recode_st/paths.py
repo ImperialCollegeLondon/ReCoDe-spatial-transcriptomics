c9179fe864b653633baebe278552cd46e2c43899
Sara Patti <sep22@ic.ac.uk>
2025-06-11 09:04:49 +0100

    reorganzied path names, changed path for data and zarr to raise an error, not make the directory; added code to check if logging path exists and if not to make it

--- src/recode_st/paths.py
+++ src/recode_st/paths.py
@@ -5,17 +5,21 @@ from pathlib import Path
 # Set directories
 base_dir = Path(__file__).parent.parent.parent  # TODO: Make this configurable
 data_path = base_dir / "data"
-output_path = base_dir / "analysis"
 xenium_path = data_path / "xenium"
 zarr_path = data_path / "xenium.zarr"
+output_path = base_dir / "analysis"
 logging_path = output_path / "logs"
 
 # Confirm directories exist
 if not Path(base_dir).exists():
     raise FileNotFoundError(f"Input path {base_dir} does not exist.")
+if not Path(data_path).exists():
+    raise FileNotFoundError(f"Input path {base_dir} does not exist.")
+if not Path(zarr_path).exists():
+    raise FileNotFoundError(f"Input path {base_dir} does not exist.")
+
+# Make sure output directories exist
 if not Path(output_path).exists():
     output_path.mkdir(parents=True, exist_ok=True)
-if not Path(data_path).exists():
-    data_path.mkdir(parents=True, exist_ok=True)
-if not Path(zarr_path).exists():
-    zarr_path.mkdir(parents=True, exist_ok=True)
+if not Path(logging_path).exists():
+    logging_path.mkdir(parents=True, exist_ok=True)
